2635f3bc8fa0f4bfd6a8cb9c43b9810caa2c359e
saladintimur237 <saladintimur237@gmail.com>
2024-10-31 19:28:43 +0700

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOf8qEuuMnKbpIXBCZBy_jLT2thKdzDdM",
-  authDomain: "linkproject42.firebaseapp.com",
-  projectId: "linkproject42",
-  storageBucket: "linkproject42.firebasestorage.app",
-  messagingSenderId: "621067113200",
-  appId: "1:621067113200:web:4bd7c2cf71665e7453e835"
+  apiKey: "AIzaSyAd-W0DCIP_AnBHL6RGvIw8D9l6kYE_Xus",
+  authDomain: "linkproject43.firebaseapp.com",
+  projectId: "linkproject43",
+  storageBucket: "linkproject43.appspot.com",
+  messagingSenderId: "947977902417",
+  appId: "1:947977902417:web:9b4a702275bfa47cf01b70"
 };
 
 const app = initializeApp(firebaseConfig);
